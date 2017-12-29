58c670dbd89d65acb86050879d79d787e969c924
tadeubarbosa <tadeufbarbosa@gmail.com>
2017-12-29 21:15:45 -0200

    adicionando classe para autoload

--- core/App/Start.php
+++ core/App/Start.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'start';
+$corePath = dirname(__FILE__, 2);
+$corePath = str_replace('\\', '/', $corePath) . '/';
+define('CORE', $corePath);
+
+require_once 'AutoLoadClass.php';
+
+spl_autoload_register('AutoLoadClass::register');
+
+
