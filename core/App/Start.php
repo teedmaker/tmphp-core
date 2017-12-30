c72d4c6234c864ee316e88dc638fc099ac6fadcc
tadeubarbosa <tadeufbarbosa@gmail.com>
2017-12-30 09:59:44 -0200

    movendo inclusão de App/AutoLoadClass.php para o composer

--- core/App/Start.php
+++ core/App/Start.php
@@ -1,15 +1,37 @@
 <?php
 
+$requestUri = $_SERVER['REDIRECT_URL'];
+$phpSelf    = $_SERVER['PHP_SELF'];
+$phpSelf    = dirname($phpSelf, 2);
+
+$scheme   = $_SERVER['REQUEST_SCHEME'];
+$httpHost = $_SERVER['HTTP_HOST'];
+$host   = "{$scheme}://{$httpHost}{$phpSelf}/";
+define('HOST', $host);
+
+$scriptFileName = $_SERVER['SCRIPT_FILENAME'];
+$rootPath       = dirname($scriptFileName, 2);
+$base = "{$rootPath}/";
+define('BASE', $base);
+
+$branch = str_replace("{$phpSelf}/", '', $requestUri);
+$branch = trim($branch, '/');
+define('BRANCH', $branch);
+
 $core = dirname(__FILE__, 2);
 $core = str_replace('\\', '/', $core) . '/';
 define('CORE', $core);
 
 ###
 
-require_once 'AutoLoadClass.php';
-
 spl_autoload_register('AutoLoadClass::register');
 
 ###
 
+$dotenv = new Dotenv\Dotenv(__DIR__, '../../../../../.env');
+$dotenv->load();
+
+##
+
 TMPHP\App\Brain::init();
+
