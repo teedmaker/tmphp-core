3ce8345dfc2ae0468dc7660205b35962ec39ce81
tadeubarbosa <tadeufbarbosa@gmail.com>
2018-01-05 20:29:48 -0200

    adicionando exemplificação de rota

--- core/App/Brain.php
+++ core/App/Brain.php
@@ -5,6 +5,12 @@ namespace TMPHP\App;
 class Brain
 {
 	public static function init() {
-		var_dump(BRANCH);
+		$rota = BRANCH? BRANCH: 'em branco';
+		$host = HOST;
+
+		echo "Rota aual: {$rota} <br/> <br/>";
+
+		echo "Experimente ir para:  <br/> <br/>";
+		echo "<a href=\"{$host}teste\">teste</a> <a href=\"{$host}users/teedmaker\">users/teedmaker</a> ";
 	}
 }
