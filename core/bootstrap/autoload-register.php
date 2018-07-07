13de7e8b83c46cd7546f9ced0a339581c9812b69
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-07 19:14:18 -0300

    Remove Autoload class

--- core/bootstrap/autoload-register.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../App/AutoLoadClass.php';
-
-spl_autoload_register('AutoLoadClass::register');
--- core/bootstrap/start.php
+++ core/bootstrap/start.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once 'defines.php';
-require_once 'autoload-register.php';
 require_once 'config-env-data.php';
 
 TMPHP\App\Brain::init();
