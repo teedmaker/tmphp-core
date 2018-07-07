87eb56c28cd9017e30fcce5d572575f5be267d94
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-07 20:50:40 -0300

    Add some defines

--- src/bootstrap/defines.php
+++ src/bootstrap/defines.php
@@ -1,23 +1,28 @@
 <?php
 
-$httpHost = $_SERVER['HTTP_HOST'];
-$phpSelf    = $_SERVER['PHP_SELF'];
-$phpSelf    = dirname($phpSelf, 2);
+// getting a method from actual access (e.g.: get/post/put/delete)
+$method = strtolower($_SERVER['REQUEST_METHOD']);
+define('METHOD', $method);
 
-$scheme   = $_SERVER['REQUEST_SCHEME'];
-$httpHost = $_SERVER['HTTP_HOST'];
-$host   = "{$scheme}://{$httpHost}{$phpSelf}/";
+// getting actual host
+$phpSelf  = dirname($_SERVER['PHP_SELF'], 2);
+$host     = "{$_SERVER['HTTP_HOST']}{$phpSelf}";
+$host     = str_replace('//', '/', $host);
+$host     = trim($host, '/');
+$host     = "{$_SERVER['REQUEST_SCHEME']}://{$host}/";
 define('HOST', $host);
 
-$scriptFileName = $_SERVER['SCRIPT_FILENAME'];
-$rootPath       = dirname($scriptFileName, 2);
-$base = "{$rootPath}/";
+// getting path from general files
+$base = dirname($_SERVER['SCRIPT_FILENAME'], 2) . '/';
 define('BASE', $base);
 
-$branch = str_replace("{$phpSelf}/", '', $httpHost);
-$branch = trim($branch, '/');
-define('BRANCH', $branch);
-
+// getting path from core of this project
 $core = dirname(__FILE__, 2);
 $core = str_replace('\\', '/', $core) . '/';
 define('CORE', $core);
+
+// getting actual branch (e.g.: site.com/my/actual/branch)
+$branch = str_replace("{$phpSelf}/", '', $_SERVER['REQUEST_URI']);
+$branch = trim($branch, '/');
+$branch = empty($branch)? '/': $branch;
+define('BRANCH', $branch);
