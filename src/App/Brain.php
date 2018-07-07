df624a037181793864d5c7194c67479d32be7ee6
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-07 20:53:02 -0300

    Remove test output in App/Brain.php

--- src/App/Brain.php
+++ src/App/Brain.php
@@ -5,12 +5,5 @@ namespace TMPHP\App;
 class Brain
 {
     public static function init() {
-        $rota = BRANCH? BRANCH: 'em branco';
-        $host = HOST;
-
-        echo "Rota aual: {$rota} <br/> <br/>";
-
-        echo "Experimente ir para:  <br/> <br/>";
-        echo "<a href=\"{$host}teste\">teste</a> <a href=\"{$host}users/teedmaker\">users/teedmaker</a> ";
     }
 }
