3f75c1998622bb055c5d537979ff9ba5944b022a
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-07 19:49:14 -0300

    Change tab indentation to space in .editorconfig
    - Correct indentation in all files

--- core/App/Brain.php
+++ core/App/Brain.php
@@ -4,13 +4,13 @@ namespace TMPHP\App;
 
 class Brain
 {
-	public static function init() {
-		$rota = BRANCH? BRANCH: 'em branco';
-		$host = HOST;
+    public static function init() {
+        $rota = BRANCH? BRANCH: 'em branco';
+        $host = HOST;
 
-		echo "Rota aual: {$rota} <br/> <br/>";
+        echo "Rota aual: {$rota} <br/> <br/>";
 
-		echo "Experimente ir para:  <br/> <br/>";
-		echo "<a href=\"{$host}teste\">teste</a> <a href=\"{$host}users/teedmaker\">users/teedmaker</a> ";
-	}
+        echo "Experimente ir para:  <br/> <br/>";
+        echo "<a href=\"{$host}teste\">teste</a> <a href=\"{$host}users/teedmaker\">users/teedmaker</a> ";
+    }
 }
--- core/bootstrap/routes.php
+++ core/bootstrap/routes.php
@@ -5,7 +5,7 @@ require_once '../Routes/Route.php';
 $filesRoute = glob(BASE . 'app/routes/{*,**/*}.php', GLOB_BRACE);
 
 foreach($filesRoute as $file) {
-	require_once $file;
+    require_once $file;
 }
 
 Route::findActualRoute();
