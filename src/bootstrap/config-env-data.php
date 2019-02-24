4c9b87229616b6364b62a9e3c48abc7ae8c9454a
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-02-24 14:29:29 -0300

    Add a verification from .env file

--- src/bootstrap/config-env-data.php
+++ src/bootstrap/config-env-data.php
@@ -1,4 +1,10 @@
 <?php
 
-$dotenv = new Dotenv\Dotenv(__DIR__, '../../../../../.env');
+$dotenvFile = '../../../../../.env';
+
+if (!file_exists($dotenvFile)) {
+    copy(__DIR__.'/../../../../../.env.sample', __DIR__.'/../../../../../.env');
+}
+
+$dotenv = new Dotenv\Dotenv(__DIR__, $dotenvFile);
 $dotenv->load();
