eff7c0dc3df52a21904d6a9bd2c913535bd7fbf7
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-04-17 16:39:54 -0300

    Create some functions to manipulate View

--- src/Core/View.php
+++ src/Core/View.php
@@ -1,22 +1,47 @@
 <?php
 
+/**
+ * Class to manipulate view
+ */
 class View
 {
+    private $path = '';
     private $data = [];
 
+    /**
+     * Initialize View class
+     *
+     * @param string $path: the path to view
+     * @param array $data: the data to send to view
+     * @return View
+     */
     public function __construct(string $path, array $data=[]) {
+        $path = str_replace('.', '/', $path);
         $this->path = $path;
         $this->data = $data;
         return $this;
     }
 
-    public function with($name, $value) {
-        $this->data[$name] = $value;
+    /**
+     * Add variables to send to view
+     *
+     * @param string $name
+     * @param void $value
+     * @return View
+     */
+    public function with(string $name, $value) {
+        $this->data['view'][$name] = $value;
         return $this;
     }
 
-    public function title($value) {
-        $this->data['title'] = $value;
+    /**
+     * Set title to the template
+     *
+     * @param string $value
+     * @return void
+     */
+    public function title(string $title) {
+        $this->data['template']['title'] = $title;
         return $this;
     }
 }
